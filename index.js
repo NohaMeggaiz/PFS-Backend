534b1247c4f3c0301e2009cecea345f91c499b73
noha <nohameggaiz@gmail.com>
2024-04-27 10:52:13 +0000

    added cart functions

--- index.js
+++ index.js
@@ -254,10 +254,36 @@ const fetchUser = async (req, res, next) => { // Add 'next' as a parameter
 
 
 //end point for adding products in cartdata
-app.post('/addtocart',async(req,res)=>{
-   console.log(req.body);
+// Point de terminaison pour ajouter des produits dans cartdata
+app.post('/addtocart', fetchUser, async (req, res) => {
+    console.log(req.body, req.user);
+ 
+    let userData = await Users.findOne({ _id: req.user.id });
+    userData.cartData[req.body.itemId] += 1;
+    await Users.findOneAndUpdate({ _id: req.user.id }, { cartData: userData.cartData });
+    res.send("Ajouté");
+ });
+ 
+//creating endpoint to remove product from cartdata
+app.post('/removefromcart',fetchUser,async(req,res)=>{
+    console.log("removed",req.body.itemId);
+    let userData = await Users.findOne({ _id: req.user.id });
+    if(userData.cartData[req.body.itemId]>0)
+       userData.cartData[req.body.itemId] -= 1;
+    await Users.findOneAndUpdate({ _id: req.user.id }, { cartData: userData.cartData });
+    res.send("Supprimé");
 
-})
+
+});
+
+app.post('/getcart', fetchUser, async (req, res) => {
+    console.log("Récupération du panier");
+    let userData = await Users.findOne({ _id: req.user.id });
+    res.json(userData.cartData);
+});
+
+
+
 
 app.listen(port,(error)=>{
     if(!error){
